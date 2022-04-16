413324a322506ff095b3890d8336dcb657eba7f0
nguyenlong1999 <45970234+nguyenlong1999@users.noreply.github.com>
2022-04-16 14:45:49 +0700

    bai lam

--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    String mOrderMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void showDonutOrder(View view) {
         Toast.makeText(this, getString(R.string.donut_order_message), Toast.LENGTH_SHORT).show();
+        mOrderMessage = getString(R.string.donut_order_message);
     }
 
     public void showIceCreamOrder(View view) {
         Toast.makeText(this, getString(R.string.ice_cream_order_message), Toast.LENGTH_SHORT).show();
+        mOrderMessage = getString(R.string.ice_cream_sandwiches);
 
     }
 
     public void showFroyoOrder(View view) {
         Toast.makeText(this, getString(R.string.froyo_order_message), Toast.LENGTH_SHORT).show();
+        mOrderMessage = getString(R.string.froyo_order_message);
+    }
+
+    public void showOrderActivity(View view) {
+        if (mOrderMessage == "") {
+            Toast.makeText(this, "Please choose a droid desserts", Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(this, mOrderMessage, Toast.LENGTH_SHORT).show();
 
     }
 }
