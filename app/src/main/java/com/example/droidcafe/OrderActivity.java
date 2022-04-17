553e75b19dda97b428b22c5599e932996d08d178
nguyenlong1999 <45970234+nguyenlong1999@users.noreply.github.com>
2022-04-17 08:54:06 +0700

    xxxxxxxx

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
+    RadioGroup rgDelivery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +17,34 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         setTitle("Đặt hàng");
 
+        rgDelivery = findViewById(R.id.rgDelivery);
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView tvGioHang = findViewById(R.id.order_textView);
         tvGioHang.setText(message);
+
+        rgDelivery.setOnCheckedChangeListener(
+                new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                        switch (id) {
+                            case R.id.sameday:
+                                showMessage(R.string.sameday);
+                                break;
+                            case R.id.nextday:
+                                showMessage(R.string.nextday);
+                                break;
+                            case R.id.pickup:
+                                showMessage(R.string.pickup);
+                                break;
+                        }
+                    }
+                }
+        );
+    }
+
+    public void showMessage(int idMessage) {
+        Toast.makeText(this, getString(idMessage), Toast.LENGTH_SHORT).show();
     }
 }
