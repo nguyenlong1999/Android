aa3dd39ffd050dfe34360bb950bd4a7b6281a370
nguyenlong1999 <45970234+nguyenlong1999@users.noreply.github.com>
2022-04-17 10:57:59 +0700

    xxx

--- app/src/main/java/com/example/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/droidcafe/OrderActivity.java
@@ -4,12 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 public class OrderActivity extends AppCompatActivity {
     RadioGroup rgDelivery;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,7 @@ public class OrderActivity extends AppCompatActivity {
         setTitle("Đặt hàng");
 
         rgDelivery = findViewById(R.id.rgDelivery);
+        spinner = findViewById(R.id.spinner_phone);
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -42,7 +52,30 @@ public class OrderActivity extends AppCompatActivity {
                     }
                 }
         );
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long key) {
+                        List<String> listPhoneTypes = Arrays.asList(
+                                getResources().getStringArray(R.array.labels_array)
+                        );
+
+                        Toast.makeText(OrderActivity.this, listPhoneTypes.get(i), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                }
+        );
     }
+
 
     public void showMessage(int idMessage) {
         Toast.makeText(this, getString(idMessage), Toast.LENGTH_SHORT).show();
